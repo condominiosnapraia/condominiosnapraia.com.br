d236a9516cf3c30177372425f24a206dde5d64be
Condomínios na Praia <felipeabmclub@gmail.com>
2026-08-14 21:44:49 +0000

    fix: remove incompatible catchall function route

--- functions/debug-assets.js
+++
@@ -1,8 +0,0 @@
-export function onRequest({ env, next }) {
-  return new Response(JSON.stringify({
-    assets_bound: Boolean(env?.ASSETS),
-    assets_fetch_type: typeof env?.ASSETS?.fetch,
-    supabase_key_bound: Boolean(env?.SUPABASE_ANON_KEY),
-    next_type: typeof next
-  }), { headers: { 'content-type': 'application/json; charset=utf-8' } });
-}
