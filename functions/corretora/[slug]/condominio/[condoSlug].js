b3a2d36cab7e4af8a84794664cf2b27107a1a2dc
condominiosnapraia <3.02672678e+08+condominiosnapraia@users.noreply.github.com>
2026-09-14 14:20:31 +0000

    feat: adicionar rotas corretora para marcia anzolin

---
+++ functions/corretora/[slug]/condominio/[condoSlug].js
@@ -0,0 +1,7 @@
+import { onRequest as handle } from '../../../corretor/[slug]/condominio/[condoSlug].js';
+
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  url.pathname = url.pathname.replace(/^\/corretora\//, '/corretor/');
+  return handle({ ...context, request: new Request(url, context.request) });
+}
